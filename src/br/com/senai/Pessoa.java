21e141cb95c61e2dc51adf468a257c6581cd15c9
Maderalice <madrealice4@gmail.com>
2021-05-24 16:09:03 -0300

    Classe pessoa

---
+++ src/br/com/senai/Pessoa.java
@@ -0,0 +1,35 @@
+package br.com.senai;
+
+public class Pessoa {
+	
+	//ATRIBUTOS 
+	String nome;
+	int anoDeNascimento;
+	String sexo;
+	
+	
+	//METODOS
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getAnoDeNascimento() {
+		return anoDeNascimento;
+	}
+	
+	public void setAnoDeNascimento(int anoDeNascimento) {
+		this.anoDeNascimento = anoDeNascimento;
+	}
+	
+	public String getSexo() {
+		return sexo;
+	}
+	
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+}
